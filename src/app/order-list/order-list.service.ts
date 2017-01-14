2892c1a412b201ace92b4b9f2964c3032e36fd6e
Felipe Palazzo <palazzofelipe@gmail.com>
2017-01-14 18:12:50 -0200

    Create order service

---
+++ src/app/order-list/order-list.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Order } from '../../app/models/order.interface';
+
+const ORDERS_URL: string = './orders.json';
+
+@Injectable()
+export class OrderListService {
+  constructor(private http: Http){}
+
+  getOrders(): Observable<Order[]> {
+    return this.http
+      .get(ORDERS_URL)
+      .map((response: Response) => response.json());
+  }
+}
