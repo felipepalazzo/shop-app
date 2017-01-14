38684358e7e766d83cf70fad0ea8a6bdcc025915
Felipe Palazzo <palazzofelipe@gmail.com>
2017-01-14 17:17:49 -0200

    Add top menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
   ],
   imports: [
     BrowserModule,
